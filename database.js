586bfe781ac70f74d097f9149fa1c0df740aac65
kikedare78@gmail.com <kikedare78@gmail.com>
2024-12-14 12:16:43 +0000

    Added Database.js

---
+++ database.js
@@ -0,0 +1,17 @@
+
+import mysql from "mysql2";
+import dotenv from "dotenv"; 
+
+dotenv.config() 
+
+// Create connection pool 
+//Environment Variable  - Install the dotenv library by using npmidotenv - and create file name ENV - 
+ const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST, 
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise();
+
+//Export: export default db; allows easy importing in other files
+export default db;
