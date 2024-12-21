273de270faec092336b5f646cc66a28503827076
Nick Campbell <nickcampbell@monzo.com>
2024-12-21 12:53:57 +0000

    Add biome linter

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-import express from "express";
+import path from "node:path"; // Node.js's built-in path module, which provides utilities for working with file and directory paths.
 import bodyParser from "body-parser";
 import ejs from "ejs";
-import path from "path"; // Node.js's built-in path module, which provides utilities for working with file and directory paths.
+import express from "express";
 
-// import the CRUD from the routes folder for each of the entities. 
+// import the CRUD from the routes folder for each of the entities.
 
-import graduatesRoutes from "./routes/graduates.js";
-import companiesRoutes from "./routes/companies.js";
-import jobsRoutes from "./routes/jobs.js";
-import applicationsRoutes from "./routes/applications.js";
 import adminRoutes from "./routes/admin.js";
+import applicationsRoutes from "./routes/applications.js";
+import companiesRoutes from "./routes/companies.js";
+import graduatesRoutes from "./routes/graduates.js";
+import jobsRoutes from "./routes/jobs.js";
 import paymentsRoutes from "./routes/payments.js";
 
 const app = express();
-const port = 3000; 
+const port = 3000;
 
 // Set EJS as the view engine - this is for the Front-End
 app.set("view engine", "ejs");
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-//Routes/Endpoints 
+//Routes/Endpoints
 app.use("/graduates", graduatesRoutes);
 app.use("/companies", companiesRoutes);
 app.use("/jobs", jobsRoutes);
@@ -31,17 +31,15 @@ app.use("/applications", applicationsRoutes);
 app.use("/admin", adminRoutes);
 app.use("/payments", paymentsRoutes);
 
-//Homepage Route -to diplay the homepage 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'public', 'index.html')); //The process.cwd() method returns the current working directory of the Node.js process.
-  });
-
+//Homepage Route -to diplay the homepage
+app.get("/", (req, res) => {
+	res.sendFile(path.join(process.cwd(), "public", "index.html")); //The process.cwd() method returns the current working directory of the Node.js process.
+});
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
-
 
 /*
 https://blog.logrocket.com/node-js-project-architecture-best-practices/
