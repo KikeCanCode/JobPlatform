5a066d2b7f46b70ab045fb616ecc4e29f2ac9b2a
Nick Campbell <nickcampbell@monzo.com>
2024-12-21 20:53:03 +0000

    Example of using ORM

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,14 +1,14 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './db/schema.js',
-  dialect: 'mysql',
-  dbCredentials: {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-  },
+	out: "./drizzle",
+	schema: "./db/schema.js",
+	dialect: "mysql",
+	dbCredentials: {
+		host: process.env.MYSQL_HOST,
+		user: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PASSWORD,
+		database: process.env.MYSQL_DATABASE,
+	},
 });
