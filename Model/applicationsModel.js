9267915575473e8b0a9bd0d446e128cf9485ce0e
kikedare78@gmail.com <kikedare78@gmail.com>
2025-01-16 18:27:11 +0000

    Added applicationsModel

---
+++ Model/applicationsModel.js
@@ -0,0 +1,58 @@
+import db from "../db/index.js";
+import { applicationsTable } from "../schema/applicationsTable.js"; // Import the table schema
+
+class Application {
+	constructor(id, jobId, graduateId, dateApplied) {
+		this.id = id;
+		this.jobId = jobId;
+		this.graduateId = graduateId;
+		this.dateApplied = dateApplied;
+	}
+
+  // Method to create a new application  
+  static async createApplicaion ({ jobId, graduateId }) {
+    try {
+        const result = await db
+        .insert(applicationsTable)
+        .values({
+            job_id: jobId,
+            graduate_id: graduateId,
+        })
+        .returning("*"); // Return the inserted application data
+    } catch (err) {
+        throw new Error(`Error creating application: ${err.message}`);
+        }
+ }
+
+ // Method to retrieve applications by graduateId
+ static async getApplicationByGraduate(graduateId) {
+    try {
+        const result = await db
+        .select()
+        .from(applicationsTable)
+        .where("graduates_id", graduateId);
+
+        return result;
+    
+    } catch (err) {
+        throw new Error (`Error retrieving applications for graduate ${graduateId}: ${err.message}`
+			);
+    }
+
+ }
+
+// Method to delete an application by applicationId
+static async deleteApplication(applicationId) {
+    try {
+        await db
+            .delete(applicationsTable)
+            .where("id", applicationId);
+
+        return { success: true, message: "Application deleted successfully!" };
+    } catch (err) {
+        throw new Error(`Error deleting application: ${err.message}`);
+        }
+    }
+}
+
+export default Application;
--- Model/graduatesModel.js
+++ Model/graduatesModel.js
@@ -1,4 +1,4 @@
-import db from "./db"; // Import your Drizzle ORM instance
+import db from "../db/index.js"; // Import your Drizzle ORM instance
 import bcrypt from "bcryptjs";
 import multer from "multer";
 
